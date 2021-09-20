0f91a9fe1114ebd0ff443dbb12f105e77921b61e
Scott Wolchok <swolchok@fb.com>
2021-09-20 16:03:20 -0700

    [PyTorch] RFC: Add tuple inline storage
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/64066
    
    I noticed a bunch of time being spent heap-allocating Tuples
    in the unpickler. 1-, 2-, and 3-element Tuples are apparently common
    enough that they get their own bytecode instructions, so I decided to
    try also giving them their own representation. We store up to 3
    IValues inline in `Tuple` rather than doing a second heap allocation
    for a `std::vector<IValue>`.
    ghstack-source-id: 138545811
    
    Differential Revision: [D30592622](https://our.internmc.facebook.com/intern/diff/D30592622/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D30592622/)!

--- torch/csrc/jit/runtime/static/impl.cpp
+++ torch/csrc/jit/runtime/static/impl.cpp
@@ -707,7 +707,7 @@ std::vector<at::Tensor> StaticModule::operator()(
   return runtime()(inps);
 }
 c10::IValue StaticModule::operator()(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs) {
   return runtime()(args, kwargs);
 }
@@ -788,7 +788,7 @@ std::vector<at::Tensor> StaticRuntime::operator()(
 }
 
 void StaticRuntime::set_inputs(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs) {
   if (!kwargs.empty()) {
     // This is not ideal
@@ -825,7 +825,7 @@ void StaticRuntime::set_inputs(
 }
 
 c10::IValue StaticRuntime::operator()(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs) {
   // We assume inference workloads, so we do not need
   // autograd. Enabling this is a significant win on dispatcher
@@ -901,7 +901,7 @@ std::string generate_latency_json(const std::string& label, double millis) {
 } // namespace
 
 void StaticRuntime::benchmark(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs,
     const int warmup_runs,
     const int main_runs,
@@ -992,7 +992,7 @@ void StaticRuntime::benchmark(
 }
 
 float StaticRuntime::benchmark_model(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs,
     const int warmup_runs,
     const int main_runs) {
@@ -1063,7 +1063,7 @@ void display_pnode_info(const ProcessedNode& pnode) {
 }
 
 void StaticRuntime::display_nodes(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs) {
   c10::InferenceMode mode;
   if (planner_) {
@@ -1095,7 +1095,7 @@ void StaticRuntime::display_nodes(
 }
 
 StaticRuntime::IndividualMetrics StaticRuntime::benchmark_individual_ops(
-    const std::vector<c10::IValue>& args,
+    c10::ArrayRef<c10::IValue> args,
     const std::unordered_map<std::string, c10::IValue>& kwargs,
     const int warmup_runs,
     const int main_runs) {
